aefd6caae1c38aa81addb5676f8e449caf5c5c3d
Cassie Smith <cassienoelle@gmail.com>
2020-09-17 22:43:20 -0400

    smooth intro title responsivity; fix script tag in html; start javascript, typing effect for intro title

--- personal-site/js/script.js
+++ personal-site/js/script.js
@@ -1,1 +1,26 @@
- 
+"use strict";
+
+window.onload = () => {
+
+  let introTitle = document.getElementById("intro-title");
+  let introTitleText = Array.from("Hello Worlds");
+  let counter = 0;
+
+  let introWrite = () => {
+
+    console.log("called");
+    introTitle.textContent += introTitleText[counter];
+    counter++;
+
+    if (counter < introTitleText.length-1) {
+      setTimeout(introWrite, 166);
+    }
+    else if (counter === introTitleText.length-1) {
+      setTimeout(introWrite, 3000);
+    }
+
+  }
+
+  introWrite();
+
+}
