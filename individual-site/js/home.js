ab4fa7ca115b1704b368636ba4c38d96128742b6
Cassie Smith <cassienoelle@gmail.com>
2020-09-19 04:05:25 -0400

    finished portal and icons

--- individual-site/js/home.js
+++ individual-site/js/home.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 let $exercises;
 let $reflections;
 let $presentation;
@@ -21,7 +21,7 @@ $(document).ready(() => {
 
 });
 
-/*
+
 $exercises.draggable();
 $reflections.draggable();
 $presentation.draggable();
@@ -30,3 +30,77 @@ $about.draggable();
 
 $portal.droppable();
 */
+
+window.onload = () => {
+
+  let exercises = document.getElementById("exercises");
+  let reflections = document.getElementById("reflections");
+  let presentation = document.getElementById("presentation");
+  let project = document.getElementById("project");
+  let about = document.getElementById("about");
+  let portal = document.getElementById("vortex");
+  let exercisesOverlay = document.getElementById("exercises-overlay");
+  let reflectionsOverlay = document.getElementById("reflections-overlay");
+  let presentationOverlay = document.getElementById("presentation-overlay");
+  let projectOverlay = document.getElementById("project-overlay");
+  let aboutOverlay = document.getElementById("about-overlay");
+  let defaultOverlay = document.getElementById("default-overlay");
+
+  let handleClick = (event) => {
+
+  }
+
+  let handlemouseOver = (event) => {
+    console.log(event.target.id);
+    clearOverlays();
+    switch (event.target.id) {
+      case "exercises":
+        exercisesOverlay.style.display = "inline-block";
+        exercisesOverlay.style.opacity = 1;
+        console.log("exercises!");
+        break;
+      case "reflections":
+        reflectionsOverlay.style.display = "inline-block";
+        reflectionsOverlay.style.opacity = 1;
+        break;
+      case "presentation":
+        presentationOverlay.style.display = "inline-block";
+        presentationOverlay.style.opacity = 1;
+        break;
+      case "project":
+        projectOverlay.style.display = "inline-block";
+        projectOverlay.style.opacity = 1;
+        break;
+      case "about":
+        aboutOverlay.style.display = "inline-block";
+        aboutOverlay.style.opacity = 1;
+        break;
+      default:
+        defaultOverlay.style.display = "inline-block";
+        defaultOverlay.style.opacity = 1;
+        break;
+    }
+  }
+
+  let handlemouseOut = (event) => {
+    clearOverlays();
+    defaultOverlay.style.display = "inline-block";
+    defaultOverlay.style.opacity = 1;
+  }
+
+  let categories = document.getElementsByClassName("category-icon");
+   for (let i = 0; i < categories.length; i++) {
+     categories[i].addEventListener('click', handleClick);
+     categories[i].addEventListener('mouseover', handlemouseOver);
+     categories[i].addEventListener('mouseout', handlemouseOut);
+   }
+
+   let overlays = document.getElementsByClassName("overlay");
+   function clearOverlays() {
+     for (let i = 0; i < overlays.length; i++) {
+       overlays[i].style.opacity = 0;
+     }
+   }
+
+
+}
