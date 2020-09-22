cb3eeac9120b72f7bb3ce7b395339da0966c6dbb
Cassie Smith <cassiesmith@Cassies-MBP.lan>
2020-09-22 15:33:47 -0400

    final updates before initial submission

--- individual-site/js/home.js
+++ individual-site/js/home.js
@@ -1,37 +1,14 @@
 "use strict";
+
 /*
-let $exercises;
-let $reflections;
-let $presentation;
-let $project;
-let $about;
-let $portal;
+  Animate a portal/vortex on homepage
+  Symbols "control" the portal, to navigate to other "destinations" (pages)
+  When the mouse hovers over or clicks on one of the symbols (icons), the portal changes color
 
-$(document).ready(() => {
-
-  $exercises = $("#exercises");
-  $reflections = $("#reflections");
-  $presentation = $("#presentation");
-  $project = $("#project");
-  $about = $("#about");
-  $portal = $("#portal");
-
-//  $exercises.click();
-
-
-});
-
-
-$exercises.draggable();
-$reflections.draggable();
-$presentation.draggable();
-$project.draggable();
-$about.draggable();
-
-$portal.droppable();
+  Colour changes are achieved through layered images or "overlays" that fade in and out
 */
 
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
 
   let exercises = document.getElementById("exercises");
   let reflections = document.getElementById("reflections");
@@ -45,49 +22,95 @@ window.onload = () => {
   let projectOverlay = document.getElementById("project-overlay");
   let aboutOverlay = document.getElementById("about-overlay");
   let defaultOverlay = document.getElementById("default-overlay");
+  let destination = document.getElementById("destination");
 
+
+  // Click events for navigation icons
+  // Portal colour changes and linked page title is displayed as "destination"
   let handleClick = (event) => {
-
-  }
-
-  let handlemouseOver = (event) => {
-    console.log(event.target.id);
-    clearOverlays();
+    destination.style.opacity = 1;
     switch (event.target.id) {
       case "exercises":
         exercisesOverlay.style.display = "inline-block";
         exercisesOverlay.style.opacity = 1;
+        destination.textContent = "exercises";
         console.log("exercises!");
         break;
       case "reflections":
         reflectionsOverlay.style.display = "inline-block";
         reflectionsOverlay.style.opacity = 1;
+        destination.textContent = "reflections";
         break;
       case "presentation":
         presentationOverlay.style.display = "inline-block";
         presentationOverlay.style.opacity = 1;
+        destination.textContent = "presentations";
         break;
       case "project":
         projectOverlay.style.display = "inline-block";
         projectOverlay.style.opacity = 1;
+        destination.textContent = "projects";
         break;
       case "about":
         aboutOverlay.style.display = "inline-block";
         aboutOverlay.style.opacity = 1;
+        destination.textContent = "about";
+        break;
+      default:
+        break;
+      }
+  }
+
+  // Mouseover events for navigation icons
+  // Portal colour changes and linked page title is displayed as "destination"
+  let handlemouseOver = (event) => {
+    console.log(event.target.id);
+    clearOverlays();
+    destination.style.opacity = 1;
+    switch (event.target.id) {
+      case "exercises":
+        exercisesOverlay.style.display = "inline-block";
+        exercisesOverlay.style.opacity = 1;
+        destination.textContent = "exercises";
+        console.log("exercises!");
+        break;
+      case "reflections":
+        reflectionsOverlay.style.display = "inline-block";
+        reflectionsOverlay.style.opacity = 1;
+        destination.textContent = "reflections";
+        break;
+      case "presentation":
+        presentationOverlay.style.display = "inline-block";
+        presentationOverlay.style.opacity = 1;
+        destination.textContent = "presentations";
+        break;
+      case "project":
+        projectOverlay.style.display = "inline-block";
+        projectOverlay.style.opacity = 1;
+        destination.textContent = "projects";
+        break;
+      case "about":
+        aboutOverlay.style.display = "inline-block";
+        aboutOverlay.style.opacity = 1;
+        destination.textContent = "about";
         break;
       default:
         defaultOverlay.style.display = "inline-block";
         defaultOverlay.style.opacity = 1;
+        setTimeout(function(){ destination.textContent = "unknown"; }, 300);
         break;
     }
   }
 
+  // Reset on mouse out
   let handlemouseOut = (event) => {
     clearOverlays();
     defaultOverlay.style.display = "inline-block";
     defaultOverlay.style.opacity = 1;
+    destination.style.opacity = 0;
   }
 
+  // Add event listeners to all the homepage icons (symbols)
   let categories = document.getElementsByClassName("category-icon");
    for (let i = 0; i < categories.length; i++) {
      categories[i].addEventListener('click', handleClick);
@@ -95,6 +118,8 @@ window.onload = () => {
      categories[i].addEventListener('mouseout', handlemouseOut);
    }
 
+   // Clear coloured overlays and revert portal to white
+   // when the mouse isn't over an icon
    let overlays = document.getElementsByClassName("overlay");
    function clearOverlays() {
      for (let i = 0; i < overlays.length; i++) {
@@ -103,4 +128,4 @@ window.onload = () => {
    }
 
 
-}
+});
--- individual-site/js/script.js
+++ individual-site/js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/*
+  Hello World(s) typing animation for landing page
+*/
+
 window.onload = () => {
 
   introPage();
@@ -11,48 +15,59 @@ function introPage() {
   let introWorlds = document.getElementById("worlds");
   let introLink = document.getElementById("intro-link");
   let blinkingCursor = document.querySelector(".blinking-cursor");
-  // let introCursor = document.getElementById("blinking-cursor");
+
+  // Convert to arrays
   let worldsText = Array.from("worlds");
   let helloText = Array.from("hello ");
+  // Keep track of how many characters and strings we've iterated through
   let charCount = 0;
   let strCount = 0;
 
+  // Store function callbackswith parameters specific to each word
   let titleA = () => setTimeout(function(){ introWrite(helloText, introHello, 250, 1000); }, 4000);
   let titleB = () => setTimeout(function(){ introWrite(worldsText, introWorlds, 250, 4000); }, 500);
   let titles = [titleA, titleB];
 
   titles[strCount]();
 
+  // introWrite
+  //
+  // Simulate typing effect
   function introWrite(text, el, t1, t2) {
 
-    console.log("el: " + el);
-    console.log(el.textContent);
     el.textContent += text[charCount];
     charCount++;
 
+    // Inject characters into html one at a time, calling function recursively
     if (charCount < text.length-1) {
       setTimeout(function(){ introWrite(text, el, t1, t2); }, t1);
       // introCursor.style.color = "var(--intro-title-color)";
     }
+    // The final character in each word is typed on a delay
     else if (charCount === text.length-1) {
       setTimeout(function(){ introWrite(text, el, t1, t2); }, t2);
       if (strCount === titles.length-1) {
         setTimeout(styleIntroLink, t2);
       }
     }
+    // When the word is complete,
+    // reset character count and increase string count
     else if (charCount === text.length) {
       charCount = 0;
       strCount++;
+      // If this is not the last string
+      // call the function again with parameters for the next string
       if (strCount < titles.length) {
         titles[strCount]();
-      }
-      else {
-        //setTimeout(styleIntroLink, 1000);
       }
     }
   }
 
+  // Remove blinking cursor, underline animation effect on
+  // "words" to reveal link
   function styleIntroLink() {
+    // "pseudo" class added to relevant elements to control certain
+    // transitions/animations since pseudo-elements can't be accessed
     introLink.classList.add("pseudo");
     blinkingCursor.style.opacity = 0;
     introLink.style.webkitAnimationPlayState = "running";
